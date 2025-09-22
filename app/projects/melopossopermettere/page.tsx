d8f07afd1730a23c671a1f7e782bc3ae6f4dcec8
Jacopo <125825348+phoskee@users.noreply.github.com>
2025-09-22 20:25:12 +0200

    fix: correct className formatting in Home component

--- app/projects/melopossopermettere/page.tsx
+++ app/projects/melopossopermettere/page.tsx
@@ -2,7 +2,7 @@ import { LoanChart } from "./_components/loan-chart";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-items-center bg-slate-50 p-8 pb-20 font-sans sm:p-20">
+    <div className="justify-items-centerp-8 flex min-h-screen flex-col items-center pb-20 font-sans sm:p-20">
       <main className="flex w-full max-w-4xl flex-col space-y-16">
         <LoanChart />
       </main>
