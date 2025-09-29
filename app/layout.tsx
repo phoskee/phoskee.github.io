7c763b756ababcf4af962c16315ca4caf6f0ee3e
Jacopo <125825348+phoskee@users.noreply.github.com>
2025-09-29 23:32:34 +0200

    gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { GoogleAnalytics } from "@next/third-parties/google";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -29,6 +31,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <GoogleAnalytics gaId="G-HCZ96MB05S" />
     </html>
   );
 }
